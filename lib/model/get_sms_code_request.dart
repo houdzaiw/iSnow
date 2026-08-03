5f3e65b2db14e7a5d61af7379b68fd1b6790e55b
liqihui <522903183@qq.com>
2026-08-03 18:26:45 +0800

    add get sms code

--- lib/model/get_sms_code_request.dart
+++ lib/model/get_sms_code_request.dart
@@ -1,6 +1,53 @@
 // filepath: /Users/admin/Documents/project/isnow/lib/model/get_sms_code_request.dart
 
 import '../classes/oauth/provider/login_provider.dart';
+enum GetSMSType {
+  none,
+  sms,
+  whatsapp,
+}
+
+enum GetSMSPurpose {
+  register,
+  forgetPassword,
+  accountBinding,
+  accountChangeBinding,
+  verifyBinding,
+}
+
+extension GetSMSPurposeExt on GetSMSPurpose {
+  int get value {
+    switch (this) {
+      case GetSMSPurpose.register:
+        return 1;
+      case GetSMSPurpose.forgetPassword:
+        return 2;
+      case GetSMSPurpose.accountBinding:
+        return 3;
+      case GetSMSPurpose.accountChangeBinding:
+        return 4;
+      case GetSMSPurpose.verifyBinding:
+        return 5;
+    }
+  }
+
+  static GetSMSPurpose fromValue(int value) {
+    switch (value) {
+      case 1:
+        return GetSMSPurpose.register;
+      case 2:
+        return GetSMSPurpose.forgetPassword;
+      case 3:
+        return GetSMSPurpose.accountBinding;
+      case 4:
+        return GetSMSPurpose.accountChangeBinding;
+      case 5:
+        return GetSMSPurpose.verifyBinding;
+      default:
+        throw ArgumentError('Invalid GetSMSPurpose value: $value');
+    }
+  }
+}
 
 class GetSMSCodeRequest {
   final String phone;
